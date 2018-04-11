90b88fbe293a6f57e8d5f868fe780a00db6fde32
sasayabaku <sasayabaku@yahoo.co.jp>
2018-04-11 10:28:35 +0900

    Reset

--- camApp/src/camera.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-navigator.mediaDevices.getUserMedia({video: true})
-  .then(function(stream) {
-    document.getElementById('video').src = URL.createObjectURL(stream);
-  }).catch(function() {
-    alert('could not connect stream');
-  });
